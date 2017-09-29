202649c81c90623b6246b60332a70274e5054d2e
Paul Webanck <paul.webanck@edhec.com>
2017-09-29 16:43:08 +0200

    basic functionnalities are ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title:    Faker::Company.catch_phrase,
+    content: Faker::Lorem.paragraph(2)
+  )
+  article.save!
+end
+
